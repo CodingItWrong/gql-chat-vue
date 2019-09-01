5a655253090f3d2d749c3a9b3404c6066aeefbe1
Josh Justice <josh.justice@accesscfa.com>
2019-09-01 06:44:30 -0400

    Set up chat client

---
+++ src/components/Conversation.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <form @submit.prevent="sendMessage">
+      <input v-model="messageText" />
+      <button>Send</button>
+    </form>
+    <ul>
+      <li v-for="message in messages" :key="message.id">
+        {{ message.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+const LOAD_MESSAGES_QUERY = gql`
+  query {
+    messages {
+      _id
+      text
+    }
+  }
+`
+
+const SEND_MESSAGE_MUTATION = gql`
+  mutation($text: String!) {
+    sendMessage(text: $text) {
+      _id
+      text
+    }
+  }
+`
+
+const NEW_MESSAGE_SUBSCRIPTION = gql`
+  subscription restaurantAdded {
+    messageSent {
+      _id
+      text
+    }
+  }
+`
+
+export default {
+  name: 'Conversation',
+  data() {
+    return {
+      messageText: '',
+    }
+  },
+  methods: {
+    sendMessage() {
+      this.$apollo
+        .mutate({
+          mutation: SEND_MESSAGE_MUTATION,
+          variables: {
+            text: this.messageText,
+          },
+        })
+        .then(() => {
+          this.messageText = ''
+        })
+    },
+  },
+  apollo: {
+    messages: {
+      query: LOAD_MESSAGES_QUERY,
+      subscribeToMore: {
+        document: NEW_MESSAGE_SUBSCRIPTION,
+        updateQuery: (previousResult, { subscriptionData }) => {
+          console.log({ previousResult, subscriptionData })
+          const newMessage = { ...subscriptionData.data.messageSent }
+          return {
+            messages: [...previousResult.messages, newMessage],
+          }
+        },
+      },
+    },
+  },
+}
+</script>
